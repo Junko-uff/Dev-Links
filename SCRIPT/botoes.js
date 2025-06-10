cc3cb30f96aaa382e275595082cf4ae1ae95044a
Laboratório de Informática 3 - SENAI Sinop <lab03.snp@alunosenai.mt>
2025-06-10 10:54:35 -0400

    initial commit

---
+++ SCRIPT/botoes.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+   const html = document.documentElement
+   html.classList.toggle("light")
+
+    const img = document.quer
+
+   if(body.classListList.contains('light')) {
+    html.classList.remove('light')
+    img.set
+   } else {
+    html.classList.add('light')
+   }
+}
